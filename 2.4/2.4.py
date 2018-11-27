5c7d00a6b5c506500df978e55f952663fa485547
YakovFat <44905135+YakovFat@users.noreply.github.com>
2018-11-27 22:01:51 +0300

    gotovo

--- 2.4/2.4.py
+++ 2.4/2.4.py
@@ -1,21 +1,26 @@
-import json
-news_list = []
+import xml.etree.ElementTree as ET
+
+parser = ET.XMLParser(encoding="utf-8")
+tree = ET.parse("files/newsafr.xml", parser=parser)
+descriptions = []
 news = []
-with open('files/newsafr.json', encoding='utf-8') as datafile:
-    json_data = json.load(datafile)
-    for items in json_data["rss"]["channel"]["items"]:
-        news_list.append(items['description'].split())
-    for i in news_list:
-        for n in i:
-            if len(n) >= 6:
-                news.append(n)
-    news_d = {}.fromkeys(news, 0)
-    for a in news:
-        news_d[a] += 1
-    top_list = []
-    top_list.append(sorted(news_d, key=max, reverse=True))
-    print("Топ 10 самых популярных слов:")
-    for number, i in enumerate(top_list[0][0:10]):
-        print(number+1, i)
+top_list = []
+root = tree.getroot()
 
+xml_items = root.findall("channel/item")
 
+for item in xml_items:
+    description = item.find("description")
+    descriptions.append(description.text.split())
+for i in descriptions:
+    for n in i:
+        if len(n) >= 6:
+            news.append(n)
+news_d = {}.fromkeys(news, 0)
+for a in news:
+    news_d[a] += 1
+
+top_list.append(sorted(news_d, key=max, reverse=True))
+print("Топ 10 самых популярных слов:")
+for number, i in enumerate(top_list[0][0:10]):
+    print(number + 1, i)
