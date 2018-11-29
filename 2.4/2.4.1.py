26931db49b9e1998560883be11f3e02e7958f63a
YakovFat <44905135+YakovFat@users.noreply.github.com>
2018-11-29 21:44:35 +0300

    gotovo_2

--- 2.4/2.4.1.py
+++ 2.4/2.4.1.py
@@ -10,12 +10,14 @@ with open('files/newsafr.json', encoding='utf-8') as datafile:
     for i in news_list:
         for n in i:
             if len(n) >= 6:
-                news.append(n)
+                news.append(n.lower())
     news_d = {}.fromkeys(news, 0)
     for a in news:
         news_d[a] += 1
 
-    top_list.append(sorted(news_d, key=max, reverse=True))
+    top_list = sorted(news_d.items(), key=lambda item: item[1], reverse=True)
     print("Топ 10 самых популярных слов:")
-    for number, i in enumerate(top_list[0][0:10]):
-        print(number + 1, i)
+
+    for number, i in enumerate(top_list[0:10]):
+        print(number + 1, i[0], '(кол-во повторений={})'.format(i[1]))
+
--- 2.4/2.4.py
+++ 2.4/2.4.py
@@ -4,7 +4,7 @@ parser = ET.XMLParser(encoding="utf-8")
 tree = ET.parse("files/newsafr.xml", parser=parser)
 descriptions = []
 news = []
-top_list = []
+
 root = tree.getroot()
 
 xml_items = root.findall("channel/item")
@@ -15,12 +15,13 @@ for item in xml_items:
 for i in descriptions:
     for n in i:
         if len(n) >= 6:
-            news.append(n)
+            news.append(n.lower())
 news_d = {}.fromkeys(news, 0)
 for a in news:
     news_d[a] += 1
 
-top_list.append(sorted(news_d, key=max, reverse=True))
+top_list = sorted(news_d.items(), key=lambda item: item[1], reverse=True)
 print("Топ 10 самых популярных слов:")
-for number, i in enumerate(top_list[0][0:10]):
-    print(number + 1, i)
+
+for number, i in enumerate(top_list[0:10]):
+    print(number + 1, i[0], '(кол-во повторений={})'.format(i[1]))
